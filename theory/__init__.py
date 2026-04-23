77d95a99bf4e86fd9c3070ae20b607dd8beacbfb
konstantinos-athan <konathap@gmail.com>
2026-04-23 17:13:33 +0300

    update c_epal theory

--- theory/__init__.py
+++ theory/__init__.py
@@ -1,5 +1,7 @@
 from .a_gel_algebra import theory as a_gel_algebra_theory
+from .c_epal import theory as c_epal_theory
 
 theory_map = {
     ("a-gel", "algebra"): a_gel_algebra_theory,
+    ("g-epal", "algebra"): c_epal_theory,
 }
